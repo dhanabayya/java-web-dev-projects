fe20fe6a0e13f116c83308e61b06418775263276
Dhana bayya <krishnabayya@Dhanas-mac-Air.local>
2025-02-06 11:24:54 -0600

    studio

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -1,8 +1,30 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MenuItem item = new MenuItem("Pancake", "Strawberries, cream, chocolate chips", 10.09);
+        MenuItem item2 = new MenuItem("Chicken Biriyani","medium spicy with naan and raitha", 15);
+        MenuItem item3 = new MenuItem("Strawberry lemonade","Strawberries with little soda", 5.50);
+        MenuItem item4 = new MenuItem("Mango Lassie","Mango with yogurt", 5.50);
+
+        Menu menu = new Menu();
+        menu.addMenuItem("Breakfast", item);
+        menu.addMenuItem("Main Course", item2);
+        menu.addMenuItem("Drink",item3);
+        menu.addMenuItem("Drink", item4);
+        ArrayList<MenuItem> items;
+
+        for(String key: menu.getMenuItems().keySet()){
+            System.out.println(key+":");
+            items = menu.getMenuItems().get(key);
+            for(int index=0; index < items.size(); index++) {
+                MenuItem item1 = items.get(index);
+                System.out.println(item1.getName()+" "+item1.getDescription()+" "+item1.getPrice());
+            }
+        }
+
     }
 }
--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -1,4 +1,35 @@
 package org.launchcode;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
 public class Menu {
+    public HashMap<String, ArrayList<MenuItem>> menuItems = new HashMap<>();
+    private ArrayList<String> categories;
+    private Date lastUpdated;
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public void addMenuItem(String category, MenuItem item) {
+        ArrayList<MenuItem> selectedItem = menuItems.get(category);
+        if(selectedItem != null) {
+            selectedItem.add(item);
+            menuItems.put(category, selectedItem);
+        } else {
+            selectedItem = new ArrayList<MenuItem>();
+            selectedItem.add(item);
+            menuItems.put(category, selectedItem);
+        }
+    }
+
+    public HashMap<String, ArrayList<MenuItem>> getMenuItems() {
+        return menuItems;
+    }
 }
